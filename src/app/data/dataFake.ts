aff386d9631d9228c7b510ee219a4808fb0c30df
JVDourado10 <jovneiva@gmail.com>
2023-09-05 15:25:06 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":'1',
+    "title": "Chega na Netflix novo Live Action de One Piece",
+    "description": "Confira os primeiros episodios episódios da série que já estão dando o que falar",
+    "photoCover": "https://t.ctcdn.com.br/9gxc6UexnXWOJPGsk8bInSmuOmQ=/1041x163:2790x1080/640x360/smart/i790338.jpeg"
+  },
+  {
+    "id":'2',
+    "title": "Novos Episódios de One Piece estreiam essa semana",
+    "description": "Confira os primeiros episodios episódios da série que já estão dando o que falar",
+    "photoCover": "https://sm.ign.com/ign_br/screenshot/default/blob_d6b9.jpg"
+  },
+  {
+    "id":'3',
+    "title": "Novos Episódios de One Piece estreiam essa semana",
+    "description": "Confira os primeiros episodios episódios da série que já estão dando o que falar",
+    "photoCover": "https://sm.ign.com/ign_br/screenshot/default/blob_d6b9.jpg"
+  },
+  {
+    "id":'4',
+    "title": "Novos Episódios de One Piece estreiam essa semana",
+    "description": "Confira os primeiros episodios episódios da série que já estão dando o que falar",
+    "photoCover": "https://sm.ign.com/ign_br/screenshot/default/blob_d6b9.jpg"
+  }
+]
